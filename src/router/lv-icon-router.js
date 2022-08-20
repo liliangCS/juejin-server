5e0fb4ef030159785140e410483fc4e4b7ff6aa1
lilian <1220795758@qq.com>
2022-08-20 23:21:49 +0800

    掘友等级

--- src/router/lv-icon-router.js
+++ src/router/lv-icon-router.js
@@ -1,7 +1,7 @@
 const Router = require("koa-router")
 const router = new Router({ prefix: "/api/lvimg" })
 
-const lvArr = [
+const lvArr1 = [
   null,
   "1.9d93e13",
   "2.99ba5b2",
@@ -13,8 +13,32 @@ const lvArr = [
   "8.c7e4c3c"
 ]
 
+const lvArr2 = [
+  null,
+  "9b2f31c663d17de59dd9e5fff272bb85",
+  "69eb0ad2f93abf938e832fe53b979c10",
+  "65a6a28f15d70e5a77bf881c5ec5340d",
+  "8c6985e2aa4c06f307ae3734da4b43ac",
+  "cf19b122213ebdbd25b66f02500b8c27",
+  "70f1e5e3a2fde62e0d623009ab80cb12"
+]
+
 router.get("/:level", (ctx, next) => {
   const { level } = ctx.request.params
-  ctx.body = { err_no: 0, imgUrl: `https://lf3-cdn-tos.bytescm.com/obj/static/xitu_juejin_web/img/lv-${lvArr[level]}.png` }
+  const imgUrl = level == 0 ? "" : `https://lf3-cdn-tos.bytescm.com/obj/static/xitu_juejin_web/img/lv-${lvArr1[level]}.png`
+  ctx.body = {
+    err_no: 0,
+    imgUrl
+  }
 })
+
+router.get("/jc/:level", (ctx, next) => {
+  const { level } = ctx.request.params
+  const imgUrl = level == 0 ? "" : `https://lf3-cdn-tos.bytescm.com/obj/static/xitu_juejin_web/${lvArr2[level]}.svg`
+  ctx.body = {
+    err_no: 0,
+    imgUrl
+  }
+})
+
 module.exports = router
