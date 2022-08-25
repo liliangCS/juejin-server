1241e687d2a58214f361425037f7587ad3492e59
lilian <1220795758@qq.com>
2022-08-25 16:00:07 +0800

    预请求头

--- src/middleware/setHeaders.js
+++ src/middleware/setHeaders.js
@@ -1,4 +1,12 @@
 module.exports = async function setHeaders(ctx, next) {
   ctx.set('Access-Control-Allow-Origin', '*');
+  ctx.set('Access-Control-Allow-Credentials', true);
+  ctx.set('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT, OPTIONS, PATCH');
+  ctx.set('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Authorization');
+  ctx.set("Content-Type", "application/json;charset=utf-8");
+  ctx.set("Access-Control-Max-Age", 300);
+  if (ctx.request.method === 'OPTIONS') {
+    ctx.response.body = 200
+  }
   await next();
 }
